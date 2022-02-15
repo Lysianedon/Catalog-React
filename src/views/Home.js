5c9b3fa2d836fb262cc24383865d500b2bfecfd9
lysiandon <don.lysiane@gmail.com>
2022-02-15 16:25:29 +0100

    update Home css

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import films from '../films.json'
+import Film from '../views/Film'
+
 
 export default class Home extends Component {
     render() {
         return (
             <div>
 
-                <h1>Accueil</h1>
+                <h1 style={{textAlign : "center", fontSize : "3rem"}}>ACCUEIL</h1>
+
+                <ul style={{display : "flex", 
+                flexDirection : "column", 
+                justifyContent : "space-around"}}>
 
                 {
                     films.map(film => {
-                        return <ul>
-                            <li>{film.title} </li>
-                        </ul>
+                        return <li style={{margin : "1%",}}><Link to={`/film/${film.id}`} component={Film} 
+                        style={{textDecoration : "none", 
+                        color: "black", 
+                        fontWeight : "bold"}}>{film.title}</Link></li>
+                        
                     })
                 }
+                </ul>
                 
             </div>
         )
